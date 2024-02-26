e1b7de4770238bc49f79c8dfdf0ee01e0b39e6e4
Thomas Petz <tompetz2000@gmail.com>
2024-02-25 20:40:51 -0500

    Finshed A

--- exercise/a_variables/variables/src/main.rs
+++ exercise/a_variables/variables/src/main.rs
@@ -1,5 +1,10 @@
+const STARTING_MISSILES:i32 = 8;
+const READY_AMOUNT:i32 = 2;
+
 fn main() {
-    let missiles = 8;
-    let ready = 2;
+    let mut missiles:i32 = STARTING_MISSILES;
+    let ready:i32 = READY_AMOUNT;
     println!("Firing {} of my {} missiles...", ready, missiles);
+    missiles = missiles - ready;
+    println!("{} missiles left", missiles);
 }
